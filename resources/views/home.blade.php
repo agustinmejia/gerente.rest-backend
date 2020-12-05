7df9dd14ba064afe41f729716f386634781e42df
Agustin <buddy.m091@gmail.com>
2020-12-05 04:13:23 -0400

    Se agregó la plantilla del administrador.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"> <i class="fas fa-tachometer-alt"></i> Dashboard</h1>
     </div>
-</div>
+
+    @section('css')
+        
+    @endsection
+
+    @section('script')
+        
+    @endsection
 @endsection
